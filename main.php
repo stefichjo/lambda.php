db4955d53d3ea20ca1c3860688f12d2103dbff80
Stephan Schneider <stefichjo@yahoo.de>
2014-11-28 08:47:56 +0100

    hard-wired autoCurry

--- main.php
+++ main.php
@@ -15,14 +15,33 @@ function autoCurry3(Closure $f) {
 	};
 }
 
-$add3 = function($a, $b, $c) { return $a + $b + $c; };
-echo $add3(1, 2, 4);
+function autoCurry2(Closure $f) {
+	return function($a) use ($f) {
+		return function($b) use ($f, $a) {
+			return $f($a, $b);
+		};
+	};
+}
 
+/*
+	$add3 = function($a, $b, $c) { return $a + $b + $c; };
+	echo $add3(1, 2, 4);
+*/
 
-$add3curried = autoCurry3($add3);
+/*
+	$add2 = function($a, $b) { return $a + $b; };
+	$add2curried = autoCurry2($add2);
 
-$addFirst = $add3curried(1);
-$addSecond = $addFirst(2);
-$addThird = $addSecond(4);
+	$inc = $add2curried(1);
+	echo $inc(42);
+*/
 
-echo $addThird;
+/*
+	$add3curried = autoCurry3($add3);
+
+	$addFirst = $add3curried(1);
+	$addSecond = $addFirst(2);
+	$addThird = $addSecond(4);
+
+	echo $addThird;
+*/
