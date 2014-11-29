f49096fc3ce8886f61852429e4b565fea8f46987
Stephan Schneider <stefichjo@yahoo.de>
2014-11-29 12:21:02 +0100

    curry -> fp

--- fp.php
+++ fp.php
@@ -7,13 +7,25 @@ function compose(Closure $f, Closure $g) {
     };
 }
 
-// a -> a
-$inc = function ($a) { return $a + 1; };
+// ((a, b, c, ...) -> z) -> (a -> b -> c -> ... -> z)
+function autoCurry(Closure $f) {
+	$autoCurryPrivate = function (Closure $f, $args, $argsCount) use (&$autoCurryPrivate) {
+		if ($argsCount > 1)	{
+			return function($arg) use ($f, $args, $argsCount, &$autoCurryPrivate) {
+				return $autoCurryPrivate($f, array_merge($args, [$arg]), $argsCount - 1);
+			};
+		} else {
+			return function($arg) use ($f, $args) {
+				return call_user_func_array($f, array_merge($args, [$arg]));
+			};
+		}
+	};
 
-// a -> a
-$square = function ($a) { return $a * $a; };
+	$argsCount = (new ReflectionMethod($f, '__invoke'))->getNumberOfParameters();
 
-// a -> a
-$squareInc = compose($square, $inc);
-
-//echo $squareInc(2) . "\n"; // -> 9
+	if ($argsCount > 0) {
+		return $autoCurryPrivate($f, [], $argsCount);
+	} else {
+		return $f;
+	}
+}
--- main.php
+++ main.php
@@ -1,6 +1,5 @@
 <?php
 require('fp.php');
-require('curry.php');
 require('adt.php');
 require('typeclass.php');
 require('maybe.php');
