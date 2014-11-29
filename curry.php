ee4ebb025a1d9063e3865f6817e798ff4de0e397
Stephan Schneider <stefichjo@yahoo.de>
2014-11-29 10:33:39 +0100

    autoCurryMain "privatized"

--- curry.php
+++ curry.php
@@ -1,23 +1,22 @@
 <?php
 function autoCurry(Closure $f) {
+	$autoCurryPrivate = function (Closure $f, $args, $argsCount) use (&$autoCurryPrivate) {
+		if ($argsCount > 1)	{
+			return function($arg) use ($f, $args, $argsCount, &$autoCurryPrivate) {
+				return $autoCurryPrivate($f, array_merge($args, [$arg]), $argsCount - 1);
+			};
+		} else {
+			return function($arg) use ($f, $args) {
+				return call_user_func_array($f, array_merge($args, [$arg]));
+			};
+		}
+	};
+
 	$argsCount = (new ReflectionMethod($f, '__invoke'))->getNumberOfParameters();
 
 	if ($argsCount > 0) {
-		return autoCurryMain($f, [], $argsCount);
+		return $autoCurryPrivate($f, [], $argsCount);
 	} else {
 		return $f;
 	}
 }
-
-function autoCurryMain(Closure $f, $args, $argsCount) {
-	if ($argsCount > 1)	{
-		return function($arg) use ($f, $args, $argsCount) {
-			return autoCurryMain($f, array_merge($args, [$arg]), $argsCount - 1);
-		};
-	} else {
-		return function($arg) use ($f, $args) {
-			return call_user_func_array($f, array_merge($args, [$arg]));
-		};
-	}
-}
-
