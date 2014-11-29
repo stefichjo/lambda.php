cf30a11a2ec386b72a885ca5e4a9e35ed55961e4
Stephan Schneider <stefichjo@yahoo.de>
2014-11-29 01:34:34 +0100

    auto currying :)

--- curry.php
+++ curry.php
@@ -1,62 +1,25 @@
 <?php
-$add3 = function($a, $b, $c) { return $a + $b + $c; };
-
-function autoCurry(Closure $f) {
-    $args = argsCount($f); var_dump($args);
-    $argsTail = array();
-
-    if (argsCount($f) < 2) {
-        return $f;
-    } else {
-        return function($argsTail) {
-            
-        };
-    }
-}
-
-curry($add3, 42);
-
-
-
-
-
-$add2ReadyOld = curryPrepareOld($add3);
-$add2Old = $add2ReadyOld(1);
-echo $add2Old(13, 23) . "\n";
-
-$add1ReadyOld = curryPrepareOld($add2Old);
-$add1Old = $add1ReadyOld(2);
-echo $add1Old(42) . "\n";
-
-function autoCurry(Closure $f) {
-    echo argsCount($f) . "\n";
-}
 
 function argsCount(Closure $f) {
     return (new ReflectionMethod($f, '__invoke'))->getNumberOfParameters();
 }
 
-function curryOld(Closure $f, $arg) {
-    return function () use ($f, $arg) {
-        $args = func_get_args();
-        array_unshift($args, $arg);
-        return call_user_func_array($f, $args);
-    };
-}
-function curryPrepareOld(Closure $f) {
-    return function($arg) use ($f) {
-        return curryOld($f, $arg);
-    };
-};
-function autoCurryOld(Closure $f) {
-    //echo argsCount($f) . "\n";
-    if (argsCount($f) < 2) {
-        return $f;
-    } else {
-        return function($arg) use ($f) {
-            autoCurryOld(curry($f, $arg));
-        };
-    }
+function autoCurry(Closure $f) {
+	return autoCurryOnce($f, [], argsCount($f));
 }
 
+function autoCurryOnce(Closure $f, $args, $argsCount) {
+	if ($argsCount > 1)
+	{
+		return function($arg) use ($f, $args, $argsCount) {
+			return autoCurryOnce($f, array_merge([$arg], $args), $argsCount - 1);
+		};
+	}
+	else
+	{
+		return function($arg) use ($f, $args) {
+			return call_user_func_array($f, array_reverse(array_merge([$arg], $args)));
+		};
+	}
+}
 
--- main.php
+++ main.php
@@ -1,47 +1,19 @@
 <?php
 require('adt.php');
-//require('curry.php');
+require('curry.php');
 require('fp.php');
 require('monad.php');
 require('typeclass.php');
 
-function autoCurry3(Closure $f) {
-	return function($a) use ($f) {
-		return function($b) use ($f, $a) {
-			return function($c) use ($f, $a, $b) {
-				return $f($a, $b, $c);
-			};
-		};
-	};
-}
+$add = autoCurry(
+	function($a, $b) { return $a + $b; }
+);
+$inc = $add(1);
+var_dump($inc(42));
 
-function autoCurry2(Closure $f) {
-	return function($a) use ($f) {
-		return function($b) use ($f, $a) {
-			return $f($a, $b);
-		};
-	};
-}
-
-/*
-	$add3 = function($a, $b, $c) { return $a + $b + $c; };
-	echo $add3(1, 2, 4);
-*/
-
-/*
-	$add2 = function($a, $b) { return $a + $b; };
-	$add2curried = autoCurry2($add2);
-
-	$inc = $add2curried(1);
-	echo $inc(42);
-*/
-
-/*
-	$add3curried = autoCurry3($add3);
-
-	$addFirst = $add3curried(1);
-	$addSecond = $addFirst(2);
-	$addThird = $addSecond(4);
-
-	echo $addThird;
-*/
+$foo = autoCurry(
+	function($a, $b, $c) { return ($a + $b) * $c; }
+);
+$foo1 = $foo(1);
+$foo12 = $foo1(2);
+var_dump($foo12(3));
